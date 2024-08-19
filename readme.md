74e63b64a968604f4d18580f2242e0ca6eefd4cf
Jason Weaver <indyplanets@gmail.com>
2024-08-19 10:25:36 -0500

    Add solution and readme

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Rearrange Words in a Sentence with Rust
+
+## Problem
+Given a sentence text (A sentence is a string of space-separated words) in the following format:
+
+First letter is in upper case.
+Each word in text are separated by a single space.
+Your task is to rearrange the words in text such that all words are rearranged in an increasing order of their lengths. If two words have the same length, arrange them in their original order.
+
+Return the new text following the format shown above.
+
+[https://leetcode.com/problems/rearrange-words-in-a-sentence/description/](https://leetcode.com/problems/rearrange-words-in-a-sentence/description/)
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,44 @@
+// Rearrange the words in text such that all words are rearranged in an increasing order of their lengths.
+// If two words have the same length, arrange them in their original order.
+// https://leetcode.com/problems/rearrange-words-in-a-sentence/description/
+
 fn main() {
-    let text = String::from("Well my my hello there my fiend");
+    let text = String::from("Well hello to you my friend");
     rearrange(text);
 }
 
+fn upper_first_char(s: &str) -> String {
+    let mut chars = s.chars();
+    let c = chars.next().unwrap().to_uppercase().to_string();
+    let s: String = chars.collect();
+    c + &s
+}
+
 fn rearrange(text: String) -> String {
-    text
+    let words: Vec<_> = text.split(' ').collect();
+    let mut words = words.into_iter().map(|w| w.to_lowercase()).collect::<Vec<_>>();
+    words.sort_by_key(|w| w.len());
+    words[0] = upper_first_char(&words[0]);
+    words.join(" ")
 }
 
 #[test]
-fn string_is_returned() {
-    let text = String::from("Well my my hello there my fiend");
+fn rearrange_text_version_one() {
+    let text = String::from("Leetcode is cool");
     let result = rearrange(text);
-    assert_eq!(result, "Well my my hello there my fiend");
+    assert_eq!(result, "Is cool leetcode");
+}
+
+#[test]
+fn rearrange_text_version_two() {
+    let text = String::from("Keep calm and code on");
+    let result = rearrange(text);
+    assert_eq!(result, "On and keep calm code");
+}
+
+#[test]
+fn rearrange_text_version_three() {
+    let text = String::from("To be or not to be");
+    let result = rearrange(text);
+    assert_eq!(result, "To be or to be not");
 }
